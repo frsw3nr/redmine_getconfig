d9ae7d87dda5e49b645c4ca8156a2b77a1a1280c
frsw3nr <frsw3nr@gmail.com>
2017-02-22 06:30:45 +0900

    v0.1.8

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@
 # See: http://guides.rubyonrails.org/routing.html
 get 'test_result', :to => 'test_result#index'
 get 'node/:node_name/test_result', :to => 'test_result#index'
+get 'inventories', :to => 'inventories#index'
--- init.rb
+++ init.rb
@@ -1,11 +1,18 @@
+require 'redmine'
+
 Redmine::Plugin.register :redmine_getconfig do
-  name 'Redmine Getconfig plugin'
+
+  name 'Getconfig'
   author 'Minoru Furusawa'
   description 'gradle server acceptance plugin for Redmine'
   version '0.1.8'
   url 'http://github.com/frsw3nr/getconfig'
   author_url 'http://github.com/frsw3nr/'
+
   # permission :inventory, { :test_result => [:index] }, :public => true
   # menu :application_menu, :inventory, { :controller => 'test_result', :action => 'index' }, :caption => 'Inventory', :after => :activity, :param => :project_id
   # menu :application_menu, :redmine_getconfig, { :controller => 'test_result', :action => 'index' }, :caption => 'Inventory'
+  # menu :application_menu, :inventories, { :controller => 'inventories', :action => 'index' }, :caption => 'Inventory'
+  # menu :project_menu, :inventories, { :controller => 'inventories', :action => 'index' }, :caption => 'Inventory', :after => :activity, :param => :project_id
+
 end
