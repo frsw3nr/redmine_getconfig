039255d68fb8f4c2c1f79eabb4eb11178c314544
frsw3nr <frsw3nr@gmail.com>
2017-02-28 06:35:39 +0900

    v0.1.8

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -9,6 +9,8 @@ class InventoryController < ApplicationController
     # end
 
     node_name = params['server_name']
+    @node = Node.find_by(node_name: node_name)
+    @devices = Metric.joins(:nodes).where(nodes: {node_name: node_name}, device_flag: 1).distinct
     @inventories = Node.find_by(node_name: node_name).test_results
   end
 end
