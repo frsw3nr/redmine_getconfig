ddb1edaf5ea95424316dbb6ef8bf9b3adff7ea3f
frsw3nr <frsw3nr@gmail.com>
2017-03-01 06:20:18 +0900

    v0.1.8

--- app/controllers/device_inventory_controller.rb
+++ app/controllers/device_inventory_controller.rb
@@ -2,12 +2,23 @@ class DeviceInventoryController < ApplicationController
   unloadable
 
   def index
-    binding.pry
 
     node_name   = params['server_name']
     metric_name = params['metric_name']
 
-    metric = Metric.find_by(metric_name: metric_name)
-    @devices = Node.find_by(node_name: node_name).device_results.where(metric_id: metric.id).order(seq: :desc)
+    metric = Metric.joins(:nodes).find_by(nodes: { node_name: node_name}, metric_name: metric_name)
+
+    @tables = []
+    Node.find_by(node_name: node_name).device_results.where(metric_id: metric.id).order(seq: :asc).group_by(&:seq).each do |seq, devices|
+      record = {}
+      record["seq"] = seq
+      devices.each do |device|
+        record[device.item_name] = device.value
+      end
+      @tables.append(record)
+    end
+
+    @headers = @tables[0].keys
+
   end
 end
--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -2,7 +2,7 @@ class InventoryController < ApplicationController
   unloadable
 
   def index
-    # binding.pry
+
     # respond_to do |format|
     #   format.html
     #   format.json { render json: TestResultDatatable.new(view_context) }
@@ -13,4 +13,5 @@ class InventoryController < ApplicationController
     @devices = Metric.joins(:nodes).where(nodes: {node_name: node_name}, device_flag: 1).distinct
     @inventories = Node.find_by(node_name: node_name).test_results
   end
+
 end
