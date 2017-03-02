a0a0cd519377456cd7165e1c500f013bedb818af
frsw3nr <frsw3nr@gmail.com>
2017-03-03 06:31:32 +0900

    v0.1.8

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_getconfig do
   name 'Getconfig'
   author 'Minoru Furusawa'
-  description 'gradle server acceptance plugin for Redmine'
+  description 'Gradle server acceptance plugin for Redmine'
   version '0.1.8'
   url 'http://github.com/frsw3nr/redmine_getconfig'
   author_url 'http://github.com/frsw3nr/'
