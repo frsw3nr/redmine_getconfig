8f25f506ab993361792f27165f50f8da7c3ef9fc
frsw3nr <frsw3nr@gmail.com>
2017-03-13 06:11:46 +0900

    v0.1.9

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -8,12 +8,6 @@ class InventoryController < ApplicationController
     @devices = Metric.joins(:nodes).where(nodes: {node_name: node_name}, device_flag: 1).distinct
     @inventories = Node.find_by(node_name: node_name).test_results
 
-    # binding.pry
-    # respond_to do |format|
-    #   format.html
-    #   format.json { render json: TestResultDatatable.new(view_context, node_name) }
-    # end
-
   end
 
 end
--- init.rb
+++ init.rb
@@ -6,10 +6,4 @@ Redmine::Plugin.register :redmine_getconfig do
   url 'http://github.com/frsw3nr/redmine_getconfig'
   author_url 'http://github.com/frsw3nr/'
 
-#   # permission :inventory, { :test_result => [:index] }, :public => true
-#   # menu :application_menu, :inventory, { :controller => 'test_result', :action => 'index' }, :caption => 'Inventory', :after => :activity, :param => :project_id
-#   # menu :application_menu, :redmine_getconfig, { :controller => 'test_result', :action => 'index' }, :caption => 'Inventory'
-#   # menu :application_menu, :inventories, { :controller => 'inventories', :action => 'index' }, :caption => 'Inventory'
-#   # menu :project_menu, :inventories, { :controller => 'inventories', :action => 'index' }, :caption => 'Inventory', :after => :activity, :param => :project_id
-
 end
